cca95044fca155c2a24a85c23c1ab2ffcd3291aa
Nestati <38103570+BaptisteCorp@users.noreply.github.com>
2020-03-27 03:05:00 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <title>Homepage</title>
+        <meta charset="utf-8"/>
+        <link rel="stylesheet" href="style.css"/>
+    </head>
+
+    <body>
+        <div id="wrapper">
+            
+            <h1>Bienvenue dans votre cloud</h1>
+            
+            <!--<form method="POST" action="traitement.php"> -->
+                <p>
+                    <input type="text" name="pseudo" id="pseudo" placeholder="Pseudo"/>
+                    <br/>
+                    <input type="password" name="mdp" id="mdp" placeholder="Password"/>
+                    <br/>
+                    <a href="inscription.php">Vous n'avez pas encore de compte ?</a>
+                </p>
+             <!--</form> -->
+        </div>
+    </body>
+</html>
