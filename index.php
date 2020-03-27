23789b1e08d49b3881cf10bb3ab5622169ba8a9b
Pvl72 <fcplm49@gmail.com>
2020-03-27 11:10:35 +0100

    page login+homepage

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+<?php
+if (isset($_POST['mdp']) AND $_POST['mdp'] == "admin" AND isset($_POST['pseudo']) AND $_POST['pseudo'] == "admin" ){ 
+    header('Location:dashboard.php');
+}
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
     <head>
@@ -10,16 +16,20 @@
         <div id="wrapper">
             
             <h1>Bienvenue dans votre cloud</h1>
+            <img id="cloud"src="images/cloud.png" alt="icone cloud" title="cloud"/>
             
-            <!--<form method="POST" action="traitement.php"> -->
+            <form method="POST" action="index.php">
                 <p>
                     <input type="text" name="pseudo" id="pseudo" placeholder="Pseudo"/>
                     <br/>
                     <input type="password" name="mdp" id="mdp" placeholder="Password"/>
                     <br/>
-                    <a href="inscription.php">Vous n'avez pas encore de compte ?</a>
+                    <input type="submit" value="VALIDER" />
+                    <br/>
+                    <a href="inscription.php">Sign up</a>
+                    <a href="pwd_forget.php">Password forget ?</a>
                 </p>
-             <!--</form> -->
+             </form>
         </div>
     </body>
 </html>
