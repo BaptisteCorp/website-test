a5836179cac03e411546ab1980b91a6d0af20d18
Nestati <38103570+BaptisteCorp@users.noreply.github.com>
2020-03-27 18:12:43 +0100

    Add files via upload

--- database.php
+++ database.php
@@ -1,8 +1,8 @@
 <?php
-	define('HOST','localhost');
+	define('HOST','serviel.ddns.net');
 	define('DB_NAME','siteweb'); //nom de la BDD
 	define('USER','root'); //utilisateur qui se connecte
-	define('PASS',''); //pas de mdp de base
+	define('PASS','Ensimtruite.'); //pas de mdp de base
 
 	try{
 		$db = new PDO("mysql:host=" . HOST . ";dbname=" . DB_NAME, USER, PASS); #définition du chemin de la BDD
