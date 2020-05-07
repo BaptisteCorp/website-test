005add15cf08c40e6ffabfb3ab7dc59e972adafd
Nestati <38103570+BaptisteCorp@users.noreply.github.com>
2020-05-07 15:43:24 +0200

    Add files via upload

--- pwd_forget.php
+++ pwd_forget.php
@@ -61,8 +61,9 @@
 					            $hashpass = password_hash($string, PASSWORD_BCRYPT, $options);
                 				$q= $db->prepare("UPDATE users SET password = :password WHERE email = :emaile");
                         		$q->execute([
-                            		'password'=> $hashpass,
-                            		'email'=> $emaile
+                            		':password'=> $hashpass,
+                            		':emaile'=> $emaile
+
                         		]);
 					        }
         			}
